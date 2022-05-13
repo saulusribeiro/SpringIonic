54b86d5fd119e5bf11dc4e5141be8e96c802a0be
Saulus Ribeiro <saulusribeiro@gmail.com>
2022-05-13 18:52:12 -0300

    Teste da primeira classe de domínio

--- backend/springbackend/src/main/java/br/com/saulusribeiro/springbackend/resources/CategoriaResouce.java
+++ backend/springbackend/src/main/java/br/com/saulusribeiro/springbackend/resources/CategoriaResouce.java
@@ -1,17 +1,28 @@
 package br.com.saulusribeiro.springbackend.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.saulusribeiro.springbackend.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResouce {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "REST está funcionando";
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 
 }
