730d71c6640511d35a47cd4612abe32cb13786fa
Saulus Ribeiro <saulusribeiro@gmail.com>
2022-05-20 17:35:56 -0300

    Inserindo operação POST na entidade Categorias

--- backend/springbackend/src/main/java/br/com/saulusribeiro/springbackend/resources/CategoriaResource.java
+++ backend/springbackend/src/main/java/br/com/saulusribeiro/springbackend/resources/CategoriaResource.java
@@ -1,10 +1,14 @@
 package br.com.saulusribeiro.springbackend.resources;
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.saulusribeiro.springbackend.domain.Categoria;
 import br.com.saulusribeiro.springbackend.services.CategoriaService;
@@ -20,5 +24,16 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	@RequestMapping(method=RequestMethod.POST, consumes = "application/json")
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.
+				fromCurrentRequest().path("/{id}").
+				buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+				
+				
+		
 	}
 }
