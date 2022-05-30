39bff1ffb228a186ed7bc86e7e8e9ecd6d5144b4
Saulus Ribeiro <saulusribeiro@gmail.com>
2022-05-30 16:40:59 -0300

    Criando Profile de Testes

---
+++ backend/springbackend/src/main/java/br/com/saulusribeiro/springbackend/config/TestConfig.java
@@ -0,0 +1,24 @@
+package br.com.saulusribeiro.springbackend.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.saulusribeiro.springbackend.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+    private DBService dbService;
+	@Bean
+	public boolean instantiateTestDatabase() throws ParseException {
+		dbService.instantiateDatabase();
+		return true;
+	}
+
+}
