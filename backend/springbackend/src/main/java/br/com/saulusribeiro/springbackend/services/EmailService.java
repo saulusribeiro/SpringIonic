056a60887cea2302bb77f49f33ef164ffd497299
Saulus Ribeiro <saulusribeiro@gmail.com>
2022-06-04 17:07:56 -0300

    Email HTML

--- backend/springbackend/src/main/java/br/com/saulusribeiro/springbackend/services/EmailService.java
+++ backend/springbackend/src/main/java/br/com/saulusribeiro/springbackend/services/EmailService.java
@@ -1,5 +1,7 @@
 package br.com.saulusribeiro.springbackend.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import br.com.saulusribeiro.springbackend.domain.Pedido;
@@ -9,5 +11,9 @@ public interface EmailService {
 	void sendOrderConfirmation(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
