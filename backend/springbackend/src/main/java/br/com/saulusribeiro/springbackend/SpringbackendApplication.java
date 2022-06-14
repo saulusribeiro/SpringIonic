df06ca78719875dc4445294df79da8dd4ecfef20
Saulus Ribeiro <saulusribeiro@gmail.com>
2022-06-14 11:54:25 -0300

    Salvando primeiro arquivo no S3

--- backend/springbackend/src/main/java/br/com/saulusribeiro/springbackend/SpringbackendApplication.java
+++ backend/springbackend/src/main/java/br/com/saulusribeiro/springbackend/SpringbackendApplication.java
@@ -1,39 +1,18 @@
 package br.com.saulusribeiro.springbackend;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.saulusribeiro.springbackend.domain.Categoria;
-import br.com.saulusribeiro.springbackend.domain.Cidade;
-import br.com.saulusribeiro.springbackend.domain.Cliente;
-import br.com.saulusribeiro.springbackend.domain.Endereco;
-import br.com.saulusribeiro.springbackend.domain.Estado;
-import br.com.saulusribeiro.springbackend.domain.ItemPedido;
-import br.com.saulusribeiro.springbackend.domain.Pagamento;
-import br.com.saulusribeiro.springbackend.domain.PagamentoComBoleto;
-import br.com.saulusribeiro.springbackend.domain.PagamentoComCartao;
-import br.com.saulusribeiro.springbackend.domain.Pedido;
-import br.com.saulusribeiro.springbackend.domain.Produto;
-import br.com.saulusribeiro.springbackend.domain.enums.EstadoPagamento;
-import br.com.saulusribeiro.springbackend.domain.enums.TipoCliente;
-import br.com.saulusribeiro.springbackend.repositories.CategoriaRepository;
-import br.com.saulusribeiro.springbackend.repositories.CidadeRepository;
-import br.com.saulusribeiro.springbackend.repositories.ClienteRepository;
-import br.com.saulusribeiro.springbackend.repositories.EnderecoRepository;
-import br.com.saulusribeiro.springbackend.repositories.EstadoRepository;
-import br.com.saulusribeiro.springbackend.repositories.ItemPedidoRepository;
-import br.com.saulusribeiro.springbackend.repositories.PagamentoRepository;
-import br.com.saulusribeiro.springbackend.repositories.PedidoRepository;
-import br.com.saulusribeiro.springbackend.repositories.ProdutoRepository;
+import br.com.saulusribeiro.springbackend.services.S3Service;
 
 @SpringBootApplication
 public class SpringbackendApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbackendApplication.class, args);
 	}
@@ -41,7 +20,7 @@ public class SpringbackendApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-
+		  s3Service.uploadFile("C:\\temp\\fotos\\imagem-teste.jpg");
 		}
 
 }
--- backend/springbackend/src/main/java/br/com/saulusribeiro/springbackend/config/SecurityConfig.java
+++ backend/springbackend/src/main/java/br/com/saulusribeiro/springbackend/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-		    .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_POST).permitAll()
+		    .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
